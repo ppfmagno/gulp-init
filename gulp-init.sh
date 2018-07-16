78bd7ad2a06f1b003942348ae974bb50329acf15
Pedro Paulo Magno <34977611+ppfmagno@users.noreply.github.com>
2018-07-16 14:15:18 -0300

    Update gulp-init.sh

--- gulp-init.sh
+++ gulp-init.sh
@@ -1,4 +1,4 @@
-cp -n `dirname $0`/package.json `pwd`
-mkdir -p `pwd`/src/{js,scss,templates/{partials}} \
-`	``pwd`/.tmp/{css,js} \
-`	``pwd`/dist
+cp -n "`dirname $0`/package.json" "`pwd`"
+mkdir -p "`pwd`/src/{js,scss,templates/{partials}}" \
+`	`"`pwd`/.tmp/{css,js}" \
+`	`"`pwd`/dist"
